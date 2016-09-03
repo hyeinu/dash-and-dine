374e75d2f5d7a30debea7b99a1a05ee30b5dee3c
Hyein <hyeinu65@gmail.com>
2016-09-03 12:10:57 -0700

    working on the front end

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    console.log('this.state:', this.state);
     return (
       <h1 className="text-center">Welcome to React!</h1>
     );
